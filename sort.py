6e87d9072815d8e2844bc2379a521f7633405b07
alt <143414893+kodealt@users.noreply.github.com>
2024-05-29 08:47:20 -0400

    Create sort.py
    1st attempt, works.

---
+++ sort.py
@@ -0,0 +1,10 @@
+list = []
+do = True
+while do:
+    do = False
+    for i in range(len(list)-1):
+        if list[i] > list[i+1]:
+            list[i], list[i+1] = list[i+1], list[i]
+            do = True
+
+print(list)
